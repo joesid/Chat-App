4b292c895e96489215824ca0404ce15f8451c023
joesid <joesidious@gmail.com>
2021-03-21 17:43:49 +0100

    Modified

--- src/MessageList.js
+++ src/MessageList.js
@@ -1,10 +1,31 @@
 import React from 'react'
 
+const DUMMY_DATA =[
+    {
+        senderId: 'Oluwaseun',
+        text: 'How far wetyn dey sup'
+    },
+
+    {
+        senderId: 'Emeka',
+        text: 'Oboy I just dey o'
+    },
+
+    {
+        senderId: 'Oluwaseun',
+        text: 'As in eh, e no easy '
+    }
+
+]
+ 
+const dumObjects =DUMMY_DATA.map((message, index) => ({senderId: index, text:message}));
+
 class MessageList extends React.Component{
     render(){
         return(
             <div className="message-list">
-
+             
+            
             </div>
         )
     }
