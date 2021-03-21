da2c0132428f09ebf1f592e780f22d3089b2c7bb
joesid <joesidious@gmail.com>
2021-03-21 22:37:27 +0100

    edit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
 import MessageList from './MessageList.js';
+// import Chatkit from '@pusher/chatkit';
+// import { StreamChat } from 'stream-chat';
 import './App.css';
+import React from 'react';
+import NewRoomForm from './NewRoomForm.js';
+import SendMessageForm from './SendMessageForm.js';
+
+
+// class App extends React.Component{
+  
+//   componentDidMount(){
+//     const chatManager = new Chatkit.chatManger({
+      
+//     })
+//   }
+// render(){
+//   return(
+//     <div className="App">
+//       {/* <RoomList /> */}
+//     <MessageList />
+//     <SendMessageForm />
+//     <NewRoomForm />
+//     </div>
+//   )
+// }
+
+// }
+
+
+
+
 
 function App() {
   return (
